3ff036d72504c98ec2e9dcdb56d0a736ad2f727f
joshua468 <112911898+joshua468@users.noreply.github.com>
2023-10-19 21:18:19 +0100

    Add files via upload

---
+++ slack-files-bot/go.mod
@@ -0,0 +1,8 @@
+module github.com/joshua468/slack-files-bot
+
+go 1.20
+
+require (
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/slack-go/slack v0.12.3 // indirect
+)
---
+++ slack-files-bot/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/slack-go/slack"
+)
+
+func main() {
+	os.Setenv("SLACK_BOT_TOKEN", "xoxb-5900397000965-5918681742421-UmgcSxNA9VlnJ0ZHDP2Hzqew")
+	os.Setenv("CHANNEL_ID", "C05T5U63HKN")
+	api := slack.New(os.Getenv("SLACK_BOT_TOKEN"))
+	channelArr := []string{os.Getenv("CHANNEL_ID")}
+	fileArr := []string{"ZPL.pdf"}
+	for i := 0; i < len(fileArr); i++ {
+		params := slack.FileUploadParameters{
+			Channels: channelArr,
+			File:     fileArr[i],
+		}
+		file, err := api.UploadFile(params)
+		if err != nil {
+			fmt.Printf("%s\n", err)
+			return
+		}
+		fmt.Printf("Name: %s,URL:%s\n", file.Name, file.URL)
+	}
+
+}
